fb374c3e2cea815828863ceb2cc42dd67d2b7a2d
CorreyL <correylim@gmail.com>
2024-02-19 08:31:17 -0800

    Create states for tracking player scores

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import './App.css'
 
 import {
   Player,
+  Scoring,
 } from './shared.types';
 
 import Coins from './components/Score/Coins/coins';
 
+const ScoringFactory = (): Scoring => ({
+  coins: 0,
+});
+
 function App() {
   const [ currentPlayer, setCurrentPlayer ] = useState<Player>(Player.One);
+  const [ playerOneScore, setPlayerOneScore ] = useState<Scoring>(ScoringFactory());
+  const [ playerTwoScore, setPlayerTwoScore ] = useState<Scoring>(ScoringFactory());
 
   const changePlayer = (): void => {
     setCurrentPlayer(currentPlayer === Player.One ? Player.Two : Player.One);
