f962626917d9851dc5169f0e546e37bc782ea975
CorreyL <correylim@gmail.com>
2024-02-19 08:54:58 -0800

    Set up PlayerScoringContext

--- src/components/Score/Civilian/civilian.tsx
+++ src/components/Score/Civilian/civilian.tsx
@@ -1,4 +1,12 @@
+import { useContext } from 'react';
+import { PlayerScoringContext } from '../../../context/Scoring';
+
 function Civilian() {
+  const {
+    playerScore,
+    setPlayerScore,
+  } = useContext(PlayerScoringContext);
+
   return (
     <div>
       Hello World
