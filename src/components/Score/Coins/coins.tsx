8e8937a334f888f42586d1781518c7c88187d0dc
CorreyL <correylim@gmail.com>
2024-02-18 17:09:09 -0800

    Add styling to coins

--- src/components/Score/Coins/coins.tsx
+++ src/components/Score/Coins/coins.tsx
@@ -1,18 +1,19 @@
 import oneCoin from '/src/assets/one-coin.png';
 import threeCoin from '/src/assets/three-coin.png';
 import sixCoin from '/src/assets/six-coin.png';
+import './coins.css';
 
 function Coins() {
   return (
     <>
       <span>
-        <img src={oneCoin} alt="One Coin" />
+        <img src={oneCoin} className="coin one-coin" alt="One Coin" />
       </span>
       <span>
-        <img src={threeCoin} alt="Three Coin" />
+        <img src={threeCoin} className="coin" alt="Three Coin" />
       </span>
       <span>
-        <img src={sixCoin} alt="Six Coin" />
+        <img src={sixCoin} className="coin six-coin" alt="Six Coin" />
       </span>
     </>
   );
