1b24811d91981cb1f896d0944a8bde1c31a551ca
CorreyL <correylim@gmail.com>
2024-02-19 08:23:37 -0800

    Add logic to change between player 1 & 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 
+import {
+  Player,
+} from './shared.types';
+
 import Coins from './components/Score/Coins/coins';
 
 function App() {
+  const [ currentPlayer, setCurrentPlayer ] = useState<Player>(Player.One);
+
+  const changePlayer = (): void => {
+    setCurrentPlayer(currentPlayer === Player.One ? Player.Two : Player.One);
+  };
+
   return (
     <>
+      <p>Current Player: {currentPlayer}</p>
+      <button onClick={changePlayer}>Change Player</button>
       <Coins/>
     </>
-  )
+  );
 }
 
 export default App
