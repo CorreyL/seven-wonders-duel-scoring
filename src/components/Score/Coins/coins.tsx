917fd3e9b0248b2edca0bb8c223f0dba1765a7fb
CorreyL <correylim@gmail.com>
2024-02-21 17:57:31 -0800

    Put coin buttons in div
    Add padding to new div to space the coins away from top and bottom
    elements

--- src/components/Score/Coins/coins.tsx
+++ src/components/Score/Coins/coins.tsx
@@ -30,24 +30,26 @@ function Coins() {
           value={playerScore.coins}
         />
       </div>
-      <img
-        onClick={() => changeScore(1, true)}
-        src={oneCoin}
-        className="coin one-coin"
-        alt="One Coin"
-      />
-      <img
-        onClick={() => changeScore(3, true)}
-        src={threeCoin}
-        className="coin three-coin"
-        alt="Three Coin"
-      />
-      <img
-        onClick={() => changeScore(6, true)}
-        src={sixCoin}
-        className="coin six-coin"
-        alt="Six Coin"
-      />
+      <div className="coin-buttons">
+        <img
+          onClick={() => changeScore(1, true)}
+          src={oneCoin}
+          className="coin one-coin"
+          alt="One Coin"
+        />
+        <img
+          onClick={() => changeScore(3, true)}
+          src={threeCoin}
+          className="coin three-coin"
+          alt="Three Coin"
+        />
+        <img
+          onClick={() => changeScore(6, true)}
+          src={sixCoin}
+          className="coin six-coin"
+          alt="Six Coin"
+        />
+      </div>
     </div>
   );
 }
