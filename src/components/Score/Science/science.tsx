dfeffbd5defc7c47139aad5f801b514ef491a7e9
CorreyL <correylim@gmail.com>
2024-02-22 19:54:53 -0800

    Add core logic and styling for Science component
    Copied over from Civilian component, made the decision not to make an
    abstraction out of this component despite similarities, since there is
    only going to be one more type of Scoring component, Commercial, that
    has distinct scores associated with it

--- src/components/Score/Science/science.tsx
+++ src/components/Score/Science/science.tsx
@@ -1,8 +1,65 @@
+import { useContext } from 'react';
+import { PlayerScoringContext } from '../../../context/Scoring';
+
 import './science.css';
 
 function Science() {
+  const {
+    playerScore,
+    setPlayerScore,
+  } = useContext(PlayerScoringContext);
+
+  const { science: scienceScore } = playerScore;
+
+  const addScienceCard = (score: number): void => {
+    scienceScore[score] += 1;
+    setPlayerScore({
+      ...playerScore,
+      science: scienceScore,
+    });
+  };
+
+  const removeScienceCard = (score: number): void => {
+    scienceScore[score] -= 1;
+    setPlayerScore({
+      ...playerScore,
+      science: scienceScore,
+    });
+  }
+
   return (
-    <div>
+    <div className="science">
+      <div className="total-science-points">
+        {
+          Object.keys(scienceScore).map((score) => (
+            scienceScore[Number(score)] > 0
+            && (
+              [...Array(scienceScore[Number(score)])].map((_, idx) => (
+                <button
+                  key={`science-remove-btn-${score}-${idx}`}
+                  onClick={() => removeScienceCard(Number(score))}
+                  className="science-btn"
+                >
+                  {score}
+                </button>
+              ))
+            )
+          ))
+        }
+      </div>
+      <div className="add-science-points">
+        {
+          Object.keys(scienceScore).map((score, idx) => (
+            <button
+              key={`science-btn-${idx}`}
+              onClick={() => addScienceCard(Number(score))}
+              className="science-btn"
+            >
+              {score}
+            </button>
+          ))
+        }
+      </div>
     </div>
   );
 }
