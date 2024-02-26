3cd1c5dfd723a90e8439c8fe7ae377270b5bf06a
CorreyL <correylim@gmail.com>
2024-02-25 16:11:13 -0800

    Import icons for each base game guild

--- src/components/Score/GuildBase/guildBase.tsx
+++ src/components/Score/GuildBase/guildBase.tsx
@@ -1,5 +1,13 @@
 import './guildBase.css';
 
+import buildersGuildIcon from '/src/assets/guild-card-icons/builders-guild.png';
+import magistratesIcon from '/src/assets/guild-card-icons/magistrates-guild.png';
+import merchantsIcon from '/src/assets/guild-card-icons/merchants-guild.png';
+import moneylendersIcon from '/src/assets/guild-card-icons/moneylenders-guild.png';
+import scientistsIcon from '/src/assets/guild-card-icons/scientists-guild.png';
+import shipownersIcon from '/src/assets/guild-card-icons/shipowners-guild.png';
+import tacticiansIcon from '/src/assets/guild-card-icons/tacticians-guild.png';
+
 function GuildBase() {
   return (
     <div>
