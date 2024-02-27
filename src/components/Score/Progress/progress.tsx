b0360f70612c05069a108c3cc1727b0473099c6d
CorreyL <correylim@gmail.com>
2024-02-26 18:27:45 -0800

    Render Progress Token icons

--- src/components/Score/Progress/progress.tsx
+++ src/components/Score/Progress/progress.tsx
@@ -5,8 +5,26 @@ import Mathematics from '/src/assets/progress-tokens/mathematics.png';
 import Philosophy from '/src/assets/progress-tokens/philosophy.png';
 
 function Progress() {
+  const mapProgressKeysToIcons = {
+    agriculture: Agriculture,
+    philosophy: Philosophy,
+    mathematics: Mathematics,
+  };
+
   return (
     <div>
+      {
+        Object.keys(mapProgressKeysToIcons).map((progressKey) => (
+          <div
+            key={`${progressKey}-icon`}
+          >
+            <img
+              src={mapProgressKeysToIcons[progressKey as keyof typeof mapProgressKeysToIcons]}
+              alt={`${progressKey} Progress Token Icon`}
+            />
+          </div>
+        ))
+      }
     </div>
   );
 }
