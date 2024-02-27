ac7cc16b09d167ba6263414fa569706fefe451a9
CorreyL <correylim@gmail.com>
2024-02-26 18:11:31 -0800

    Import icons for Progress Tokens

--- src/components/Score/Progress/progress.tsx
+++ src/components/Score/Progress/progress.tsx
@@ -1,5 +1,9 @@
 import './progress.css';
 
+import Agriculture from '/src/assets/progress-tokens/agriculture.png';
+import Mathematics from '/src/assets/progress-tokens/mathematics.png';
+import Philosophy from '/src/assets/progress-tokens/philosophy.png';
+
 function Progress() {
   return (
     <div>
