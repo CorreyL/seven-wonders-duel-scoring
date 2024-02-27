c86c816ab087298fa7f5d35d1c1b93b44fe3a716
CorreyL <correylim@gmail.com>
2024-02-26 18:41:39 -0800

    Add @ts-expect-error decorator

--- src/components/Score/GuildBase/guildBase.tsx
+++ src/components/Score/GuildBase/guildBase.tsx
@@ -69,6 +69,7 @@ function GuildBase() {
                 setFirstInput(true);
               }}
               onChange={(e) => {
+                // @ts-expect-error e.nativeEvent does in-fact have a data key
                 const keyPressed = e.nativeEvent.data;
                 if (isNaN(Number(keyPressed))) {
                   return;
