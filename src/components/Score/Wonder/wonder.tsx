4b41bb15ac2421948a096f6074e31e65081049c8
CorreyL <correylim@gmail.com>
2024-03-03 14:59:55 -0800

    Warn user insufficient wonders were selected

--- src/components/Score/Wonder/wonder.tsx
+++ src/components/Score/Wonder/wonder.tsx
@@ -32,6 +32,19 @@ function Wonder() {
 
   return (
     <div>
+      {
+        /**
+         * @todo Prevent user from switching to the Scoring page until they've
+         * selected 4 wonders for the current player
+         */
+        ownedWonders.size !== 4
+        && (
+          <div>
+            ⚠️ Please go back to the previous page and select the 4 wonders
+            Player {currentPlayer} selected at the beginning of the game ⚠️
+          </div>
+        )
+      }
       <WonderGrid
         wondersToRender={ownedWonders}
         selectWonder={toggleWonder}
