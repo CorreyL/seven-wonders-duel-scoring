77adb4034a93a2a9ff8ca5a9046527281625d94f
CorreyL <correylim@gmail.com>
2024-03-03 12:56:31 -0800

    Render all Wonders in OwnedWonders

--- src/pages/OwnedWonders/OwnedWonders.tsx
+++ src/pages/OwnedWonders/OwnedWonders.tsx
@@ -1,8 +1,16 @@
+import WonderGrid from '../../components/WonderGrid';
+import wonderKeyToImagePath from '../../assets/wonders/wonderKeyToImagePath';
+import { WonderKeys } from '../../shared.types';
 import './OwnedWonders.css';
 
 function OwnedWonders() {
   return (
     <div>
+      <WonderGrid
+        wondersToRender={new Set(
+          Object.keys(wonderKeyToImagePath) as Array<WonderKeys>
+        )}
+      />
     </div>
   );
 }
