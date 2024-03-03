11a3600293d98030e5ece7c0b02452d7ef591062
CorreyL <correylim@gmail.com>
2024-03-03 12:41:38 -0800

    Render wonders based on passed in prop keys

--- src/components/WonderGrid/WonderGrid.tsx
+++ src/components/WonderGrid/WonderGrid.tsx
@@ -1,4 +1,6 @@
 import { WonderKeys } from '../../shared.types';
+import SelectWonder from '../SelectWonder';
+import wonderKeyToImagePath from '../../assets/wonders/wonderKeyToImagePath';
 import './WonderGrid.css';
 
 interface WonderGridProps {
@@ -8,6 +10,17 @@ interface WonderGridProps {
 function WonderGrid({ wondersToRender }: WonderGridProps) {
   return (
     <div className="wonder-grid">
+      {
+        Array.from(wondersToRender.values()).map((wonderKey, idx) => {
+          return (
+            <SelectWonder
+              key={`select-wonder-${wonderKey}-${idx}`}
+              wonderKey={wonderKey}
+              wonderImage={wonderKeyToImagePath[wonderKey]}
+            />
+          )
+        })
+      }
     </div>
   );
 }
