819b477a17eb0738e0707ba30622d6f004e9c4dd
CorreyL <correylim@gmail.com>
2024-03-03 12:36:51 -0800

    Map wonder keys to image paths

---
+++ src/assets/wonders/wonderKeyToImagePath.ts
@@ -0,0 +1,14 @@
+export default {
+  appianWay: '/src/assets/wonders/appianWay.png',
+  circusMaximus: '/src/assets/wonders/circusMaximus.png',
+  colossus: '/src/assets/wonders/colossus.png',
+  greatLibrary: '/src/assets/wonders/greatLibrary.png',
+  greatLighthouse: '/src/assets/wonders/greatLighthouse.png',
+  hangingGardens: '/src/assets/wonders/hangingGardens.png',
+  mausoleum: '/src/assets/wonders/mausoleum.png',
+  piraeus: '/src/assets/wonders/piraeus.png',
+  pyramids: '/src/assets/wonders/pyramids.png',
+  sphinx: '/src/assets/wonders/sphinx.png',
+  statueOfZeus: '/src/assets/wonders/statueOfZeus.png',
+  templeOfArtemis: '/src/assets/wonders/templeOfArtemis.png',
+};
