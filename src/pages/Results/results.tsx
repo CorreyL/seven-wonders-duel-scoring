56fccedf3babd5945db905b2f17d127df51543c1
CorreyL <correylim@gmail.com>
2024-03-04 19:28:47 -0800

    Map each Scoring key to its calc method
    Will allow for each row to be rendered programmatically by iterating
    through Object.keys(playerScores[Player.One])

--- src/pages/Results/results.tsx
+++ src/pages/Results/results.tsx
@@ -2,6 +2,15 @@ import {
   PlayerScores,
 } from '../../shared.types';
 
+import {
+  calculateDistinctScoreTotal,
+  calculateCoinsTotal,
+  calculateGuildBaseTotal,
+  calculateProgressTokensTotal,
+  calculateWonderTotal,
+  calculateTotalScore,
+} from '../../utils';
+
 import './results.css';
 
 interface ResultsProps {
@@ -9,6 +18,17 @@ interface ResultsProps {
 }
 
 function Results({ playerScores }: ResultsProps) {
+  const keyToScoringMethodMapping = {
+    civilian: calculateDistinctScoreTotal,
+    coins: calculateCoinsTotal,
+    commercial: calculateDistinctScoreTotal,
+    guildBase: calculateGuildBaseTotal,
+    military: (value: number) => (value),
+    progress: calculateProgressTokensTotal,
+    science: calculateDistinctScoreTotal,
+    wonders: calculateWonderTotal,
+  };
+
   return (
     <div>
     </div>
