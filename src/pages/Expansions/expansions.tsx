362ec149450d28774a6623dfb2d2ea91244a2a6a
CorreyL <correylim@gmail.com>
2024-03-16 08:45:49 -0700

    Render the boxarts for all 7 Wonders Duel games

--- src/pages/Expansions/expansions.tsx
+++ src/pages/Expansions/expansions.tsx
@@ -1,8 +1,15 @@
+import Agora from '/src/assets/boxart/agora.png';
+import Base from '/src/assets/boxart/base.png';
+import Pantheon from '/src/assets/boxart/pantheon.png';
+
 import './expansions.css';
 
 function Expansion() {
   return (
     <div>
+      <img src={Base}/>
+      <img src={Agora}/>
+      <img src={Pantheon}/>
     </div>
   );
 }
