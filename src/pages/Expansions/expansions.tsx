a0a97bcb74be83aaeacc7c16f1a3151fbd8b4e56
CorreyL <correylim@gmail.com>
2024-03-16 09:05:41 -0700

    Add logic to toggle Pantheon expansion

--- src/pages/Expansions/expansions.tsx
+++ src/pages/Expansions/expansions.tsx
@@ -1,3 +1,9 @@
+import {
+  useContext,
+} from 'react';
+
+import { ActivatedExpansionsContext } from '../../context';
+
 import Agora from '/src/assets/boxart/agora.png';
 import Base from '/src/assets/boxart/base.png';
 import Pantheon from '/src/assets/boxart/pantheon.png';
@@ -5,11 +11,28 @@ import Pantheon from '/src/assets/boxart/pantheon.png';
 import './expansions.css';
 
 function Expansion() {
+  const {
+    activeExpansions,
+    setActiveExpansions,
+  } = useContext(ActivatedExpansionsContext);
+
   return (
     <div className="expansions">
       <img src={Base}/>
       <img className="disabled-expansion" src={Agora}/>
-      <img src={Pantheon}/>
+      <div className="select-expansion-container">
+        <img
+          onClick={() => {setActiveExpansions((prevActiveExpansions) => ({
+            ...prevActiveExpansions,
+            pantheon: !prevActiveExpansions.pantheon,
+          }))}}
+          src={Pantheon}
+        />
+        {
+          activeExpansions.pantheon
+          && <div className="overlay"></div>
+        }
+      </div>
     </div>
   );
 }
