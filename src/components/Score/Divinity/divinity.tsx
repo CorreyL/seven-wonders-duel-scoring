b61a8345dca1afdf6772e3a5423842e8dca9692d
CorreyL <correylim@gmail.com>
2024-03-20 17:33:18 -0700

    Import and render Divinity assets

--- src/components/Score/Divinity/divinity.tsx
+++ src/components/Score/Divinity/divinity.tsx
@@ -1,8 +1,17 @@
+import Aphrodite from '/src/assets/pantheon-divinity/aphrodite.png';
+import Astarte from '/src/assets/pantheon-divinity/astarte.png';
+
 import './divinity.css';
 
 function Divinity() {
   return (
     <div>
+      <div>
+        <img src={Aphrodite}/>
+      </div>
+      <div className="divinity-selection">
+        <img src={Astarte}/>
+      </div>
     </div>
   );
 }
