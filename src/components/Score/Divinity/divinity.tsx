b6203dbf8fa9b93d47a2801994bbf5dfdcd1d4ef
CorreyL <correylim@gmail.com>
2024-03-20 17:35:22 -0700

    Render inputs for scoring Divinities
    Aphrodite is a simple 9 victory points, so a checkbox suffices to
    reflect whether a player has scored Aphrodite
    
    A player can gain 1-7 victory points from Astarte, depending on how many
    coins they used from the card, therefore it needs to be a numeric input

--- src/components/Score/Divinity/divinity.tsx
+++ src/components/Score/Divinity/divinity.tsx
@@ -1,16 +1,33 @@
+import {
+  useContext,
+} from 'react';
+import { PlayerScoringContext } from '../../../context/Scoring';
+
 import Aphrodite from '/src/assets/pantheon-divinity/aphrodite.png';
 import Astarte from '/src/assets/pantheon-divinity/astarte.png';
 
 import './divinity.css';
 
 function Divinity() {
+  const {
+    playerScore,
+  } = useContext(PlayerScoringContext);
+
   return (
     <div>
       <div>
         <img src={Aphrodite}/>
+        <input
+          checked={playerScore.divinity.aphrodite}
+          type="checkbox"
+        />
       </div>
       <div className="divinity-selection">
         <img src={Astarte}/>
+        <input
+          value={playerScore.divinity.astarte}
+          inputMode="numeric"
+        />
       </div>
     </div>
   );
