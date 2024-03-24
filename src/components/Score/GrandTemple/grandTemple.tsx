f42a3d58747dc03fc6fc734b7119efe2697f574f
CorreyL <correylim@gmail.com>
2024-03-24 10:09:09 -0700

    Add method to set grandTemple key in Player Score

--- src/components/Score/GrandTemple/grandTemple.tsx
+++ src/components/Score/GrandTemple/grandTemple.tsx
@@ -1,6 +1,25 @@
+import { useContext } from 'react';
+import { PlayerScoringContext } from '../../../context/Scoring';
+import { GrandTempleScores } from '../../../shared.types';
+
 import './grandTemple.css';
 
 function GrandTemple() {
+  const {
+    currentPlayer,
+    setPlayerScores,
+  } = useContext(PlayerScoringContext);
+
+  const setGrandTempleScore = (score: GrandTempleScores) => {
+    setPlayerScores((prevPlayerScores) => ({
+      ...prevPlayerScores,
+      [currentPlayer]: {
+        ...prevPlayerScores[currentPlayer],
+        grandTemple: score,
+      }
+    }));
+  };
+
   return (
     <div>
     </div>
