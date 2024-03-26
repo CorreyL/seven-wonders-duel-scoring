c0af57e3c866dc530b431673f6da241f52a574b4
CorreyL <correylim@gmail.com>
2024-03-26 12:57:59 -0700

    Add typescript to eslint rules

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:node/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2018,
