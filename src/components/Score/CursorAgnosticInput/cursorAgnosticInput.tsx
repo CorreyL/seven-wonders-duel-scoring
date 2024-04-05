ffe573b29ddbf94bab4bbacd4537e7d3278a9562
CorreyL <correylim@gmail.com>
2024-04-04 19:43:23 -0700

    Add scaffold for CursorAgnosticInput component

---
+++ src/components/Score/CursorAgnosticInput/cursorAgnosticInput.tsx
@@ -0,0 +1,16 @@
+/**
+ * This component provides an HTML input element that, on first input after
+ * onFocus fires, the value is replaced rather than appended to
+ *
+ * Rarely will a player need to modify their score such that they wish to append
+ * a numeric value to the default (or previously input) value, hence to reduce
+ * the number of inputs required from the user, just replace the value in the
+ * first input
+ */
+function CursorAgnosticInput() {
+  return (
+    <input/>
+  );
+}
+
+export default CursorAgnosticInput;
