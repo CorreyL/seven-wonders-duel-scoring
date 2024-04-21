b95170a04f0aa74d1375ff82251c1c4da21d8e1b
CorreyL <correylim@gmail.com>
2024-04-21 13:01:59 -0700

    Add sanctuary as selectable Pantheon wonder

--- src/assets/wonders/wonderKeyToImagePath.ts
+++ src/assets/wonders/wonderKeyToImagePath.ts
@@ -8,6 +8,7 @@ import hangingGardens from './hangingGardens.webp';
 import mausoleum from './mausoleum.webp';
 import piraeus from './piraeus.webp';
 import pyramids from './pyramids.webp';
+import sanctuary from './sanctuary.webp';
 import sphinx from './sphinx.webp';
 import statueOfZeus from './statueOfZeus.webp';
 import templeOfArtemis from './templeOfArtemis.webp';
@@ -23,6 +24,7 @@ export default {
   mausoleum,
   piraeus,
   pyramids,
+  sanctuary,
   sphinx,
   statueOfZeus,
   templeOfArtemis,
