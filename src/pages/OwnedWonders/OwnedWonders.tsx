31b2953e222de268330056c9c3e2d8f18e14123c
CorreyL <correylim@gmail.com>
2024-04-25 21:16:37 -0700

    Conditionally render Ra
    Only render Ra as an input option if Pantheon expansion is active

--- src/pages/OwnedWonders/OwnedWonders.tsx
+++ src/pages/OwnedWonders/OwnedWonders.tsx
@@ -1,5 +1,9 @@
 import { useContext } from 'react';
-import { OwnedWondersContext, PlayerScoringContext } from '../../context';
+import {
+  ActivatedExpansionsContext,
+  OwnedWondersContext,
+  PlayerScoringContext,
+} from '../../context';
 import WonderGrid from '../../components/WonderGrid';
 import wonderKeyToImagePath from '../../assets/wonders/wonderKeyToImagePath';
 import RaDivinity from '../../assets/pantheon-divinity/ra.webp';
@@ -7,6 +11,10 @@ import { WonderKeys } from '../../shared.types';
 import './OwnedWonders.css';
 
 function OwnedWonders() {
+  const {
+    activeExpansions,
+  } = useContext(ActivatedExpansionsContext);
+
   const {
     currentPlayer,
     ownedWonders,
@@ -18,7 +26,7 @@ function OwnedWonders() {
     setPlayerScores,
   } = useContext(PlayerScoringContext);
 
-  const maxWonders = (playerScore.divinity.ra) ? 5 : 4;
+  const maxWonders = (activeExpansions.pantheon && playerScore.divinity.ra) ? 5 : 4;
 
   const toggleWonder = (wonderKey: WonderKeys): void => {
     if (ownedWonders.has(wonderKey)) {
@@ -55,18 +63,24 @@ function OwnedWonders() {
   return (
     <div>
       <div>Select the {maxWonders} wonders Player {currentPlayer} chose</div>
-      <div className="flex flex-row flex-wrap justify-center items-center gap-6 p-6 m-6 divinity-selection">
-        <img
-          className="h-12"
-          src={RaDivinity}
-        />
-        <input
-          className="h-12 w-12"
-          onChange={changeMaxWonders}
-          checked={playerScore.divinity.ra as boolean}
-          type="checkbox"
-        />
-      </div>
+      {
+        activeExpansions.pantheon
+        && (
+          <div className="flex flex-row flex-wrap justify-center items-center gap-6 p-6 m-6 divinity-selection">
+            <img
+              className="h-12"
+              src={RaDivinity}
+            />
+            <input
+              className="h-12 w-12"
+              onChange={changeMaxWonders}
+              checked={playerScore.divinity.ra as boolean}
+              type="checkbox"
+            />
+          </div>
+        )
+      }
+
       <WonderGrid
         wondersToRender={new Set(
           Object.keys(wonderKeyToImagePath) as Array<WonderKeys>
