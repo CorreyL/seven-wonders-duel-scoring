0c21a31b2bfea8f2fa91961ef1d11a7beb65d90b
CorreyL <correylim@gmail.com>
2024-04-25 20:33:13 -0700

    Render input for Ra Divinity

--- src/pages/OwnedWonders/OwnedWonders.tsx
+++ src/pages/OwnedWonders/OwnedWonders.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { OwnedWondersContext } from '../../context';
 import WonderGrid from '../../components/WonderGrid';
 import wonderKeyToImagePath from '../../assets/wonders/wonderKeyToImagePath';
+import RaDivinity from '../../assets/pantheon-divinity/ra.webp';
 import { WonderKeys } from '../../shared.types';
 import './OwnedWonders.css';
 
@@ -30,6 +31,16 @@ function OwnedWonders() {
   return (
     <div>
       <div>Select the 4 wonders Player {currentPlayer} chose</div>
+      <div className="flex flex-row flex-wrap justify-center items-center gap-6 p-6 m-6 divinity-selection">
+        <img
+          className="h-12"
+          src={RaDivinity}
+        />
+        <input
+          className="h-12 w-12"
+          type="checkbox"
+        />
+      </div>
       <WonderGrid
         wondersToRender={new Set(
           Object.keys(wonderKeyToImagePath) as Array<WonderKeys>
