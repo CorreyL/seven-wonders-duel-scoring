165d1b9aefc37a3734a1918eb55c0bbef959c958
CorreyL <correylim@gmail.com>
2024-05-07 17:10:52 -0700

    Render app with react-router imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import ScoreApp from './ScoreApp.tsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ScoreApp />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ScoreApp />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
